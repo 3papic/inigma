49260de282f715b92b47ad3b8bdcdb99b98d8cd4
3papic <144006027+3papic@users.noreply.github.com>
2023-09-05 20:43:23 +0200

    Add files via upload

---
+++ dan jedan vjezba/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+function showMessage() {
+    alert('Dobrodosli na moju stranicu nadam se da ce Vam se svidjeti moja vjezba!');
+}
+
+document.getElementById('dugme1').addEventListener('click', showMessage);
